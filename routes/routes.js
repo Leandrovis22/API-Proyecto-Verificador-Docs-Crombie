a2fc9252af751f7d2709374065fe0c3af2a6d95f
Leandro <leandroviscolungo@gmail.com>
2024-08-25 22:59:37 -0300

    small name changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const loginController = require('../controllers/loginController');
+const userController = require('../controllers/userController');
 const authenticateToken = require('../middlewares/authMiddleware');
 
 // Rutas públicas
-router.get('/users', loginController.getUsers); //Solo para development se debera quitar al final
-router.post('/register', loginController.register); // Registrar un usuario, devuelve un token JWT
-router.post('/login', loginController.login); // Iniciar sesión, devuelve un token JWT
+router.get('/users', userController.getUsers); //Solo para development se debera quitar al final
+router.post('/register', userController.register); // Registrar un usuario, devuelve un token JWT
+router.post('/login', userController.login); // Iniciar sesión, devuelve un token JWT
 
 // Rutas protegidas con JWT, todo pide token
-router.get('/user', authenticateToken, loginController.getUser); // Obtener los datos del usuario
-router.get('/user/dni-images', authenticateToken, loginController.getDniImages); // Obtener las URLs firmadas de las imágenes del DNI
-router.put('/user', authenticateToken, loginController.updateUser); // Actualizar los datos del usuario
-router.delete('/user', authenticateToken, loginController.deleteUser); //Borra usuario y todos sus datos, imagenes incluidas
+router.get('/user', authenticateToken, userController.getUser); // Obtener los datos del usuario
+router.get('/user/dni-images', authenticateToken, userController.getDniImages); // Obtener las URLs firmadas de las imágenes del DNI
+router.put('/user', authenticateToken, userController.updateUser); // Actualizar los datos del usuario
+router.delete('/user', authenticateToken, userController.deleteUser); //Borra usuario y todos sus datos, imagenes incluidas
 
 module.exports = router;

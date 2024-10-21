1e5c26bfb321ec8db6f7039ecdaefd22b5b0698d
Leandro <leandroviscolungo@gmail.com>
2024-10-21 15:52:10 -0300

    arreglar routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,18 +6,24 @@ const userController = require('../controllers/userController');
 const checkController = require('../controllers/checkController');
 const { getTicket } = require('../controllers/components/ticket');
 const { getTickets } = require('../controllers/components/tickets');
-
+const Tiqueteria = require('../controllers/tiqueteriaController');
 
 router.get('/users', userController.getUsers); //Solo para development trae info de todos los user se debera quitar al final
 
 router.get('/user', authenticateToken, userController.getUser);
 
 router.post('/process-dni', authenticateToken, dniController.processDNI);
+
+// Ruta para obtener una imagen url solo de dni frente de una sola persona a la vez
+router.get('/image', authenticateToken, getTicket);
+
+// Ruta para obtener las dos imagenes url del dni frente y detras de una sola persona a la vez
 router.get('/tickets', authenticateToken, getTickets)
-router.get('/check-data', authenticateToken, checkController.checkData);	
-// router.get('/user', authenticateToken, userController.getUser); // Obtener los datos del usuario
-//router.get('/user/dni-images', authenticateToken, dniController.getDniImages); // Obtener las URLs firmadas de las imágenes del DNI
-// router.put('/user', authenticateToken, userController.updateUser); // Actualizar los datos del usuario
-// router.delete('/user', authenticateToken, userController.deleteUser); //Borra usuario y todos sus datos, imagenes incluidas
+
+router.get('/check-data', authenticateToken, checkController.checkData);
+
+// Ruta para obtener un ticket o todos los tickets segun rol
+router.get('/tiqueteria', authenticateToken, Tiqueteria.Tiqueteria) // Hay que modificar de  resultado: ticket.msqError, a resultado: ticket.resultado, 
+
 
 module.exports = router;

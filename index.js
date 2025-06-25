d1fad4b41401f492c035f7e79af1f207f9307b1f
Pablo Hernán Gómez <175459414+PabloGomez05@users.noreply.github.com>
2025-06-25 14:16:45 -0300

    Changes para poder ejecutar el local host

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const routes = require('./routes/routes');
 const authRouter = require('./routes/authRouter');
 
 app.use(cors({
-  origin: '*',
+  origin: ['http://localhost:5173'],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 86400,
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
